27246d7a29f83bb16a470d23650c8a404060a7e2
François-Xavier Bonnefont <bonnefontfx@gmail.com>
2016-03-14 10:09:53 +0100

    création d'un css connexion

--- vues/connexion.php
+++ vues/connexion.php
@@ -1,4 +1,64 @@
 <!DOCTYPE html>
+<style>
+#cover {
+  width: 100%;
+  position : absolute;
+  top:0;
+  left:0;
+  z-index: -1;
+}
+
+.image
+{
+  max-width: 100%;
+}
+
+.slogan {
+  clear: both;
+  color: white;
+  font-size: 40px;
+  font-weight: bold;
+  margin: 30px auto auto;
+  text-transform: uppercase;
+  width: 562px;
+  text-shadow: 0px 0px 4px black;
+}
+form.accueil {
+  margin: 130px auto;
+  width: 320px;
+}
+form.accueil select {
+   width: 240px;
+   height: 34px;
+   overflow: hidden;
+   background: url(new_arrow.png) no-repeat right #ddd;
+   border: 1px solid #ccc;
+}
+
+.titre
+{
+  margin-top : 500px;
+  text-align: center;
+}
+
+#titreAnnonces
+{
+  margin-top: 50px;
+}
+
+#annonces
+{
+  margin-top: 50px;
+  background-color: blue; 
+}
+
+body div div div.annonces
+{
+  margin-left: 30%;
+  display : inline-block;
+
+}
+</style>
 <html>
 <img src="images/logo.png" width="150" height="200" />
 <body>
