0d3722b30ff702de79c5b59eaa85ec99f7e2b88d
Ohlmann Anthony <anthony.ohlmann@isep.fr>
2016-03-14 09:43:23 +0100

    Changement page d'accueil

--- vues/accueil.php
+++ vues/accueil.php
@@ -1,5 +1,5 @@
 	<div id="cover">
-	<img src="static/images/moto.jpg" alt="image test" class="image" >
+	<img src="static/images/image_accueil.jpg" alt="image test" class="image" >
 	</div>
 	<div class="slogan">La Passion se partage</div>
 	<form class="accueil">
@@ -7,8 +7,25 @@
 			<option>Football</option>
 			<option>Jogging</option>
 			<option>Tennis</option>
-			<option><P></P>iscine</option>
+			<option><P></P>Piscine</option>
 		</select>
 		<input type="text">
 		<input type="submit" value="Rechercher">
 	</form>
+	<h2 class="titre">Les annonces à la mode</h2>
+	<div id="titreAnnonces">
+		<div class = "annonces">
+		Salut
+		</div>
+		<div class = "annonces">
+		Salut
+		</div>
+	</div>
+	<div id="annonces">
+		<div class = "annonces">
+		Salut
+		</div>
+		<div class = "annonces">
+		Salut
+		</div>
+	</div>
