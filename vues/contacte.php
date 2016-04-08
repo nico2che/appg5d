2ec55dc8048e7c5c761566d1b1589f38be0e94d7
chipon77 <romain.chipon77@gmail.com>
2016-04-08 11:17:22 +0200

    mise en forme formulaire contacte

--- vues/contacte.php
+++ vues/contacte.php
@@ -1,16 +1,34 @@
 <div class="fond"></div>
+<div style="text-align: center">
 <div>
 	<label for="nom">Nom :</label>
        <input type="text"/>
 </div>
 
+<br>
+
 <div>
-	<label for="prenom">Prenom :</label>
+	<label for="Email" id="Email">Email :</label>
         <input type="text" />
 </div>
 
+<br>
+
 <div>
-	<label for="Email">Email :</label>
+	<label for="Sujet" id="Sujet">Sujet :</label>
         <input type="text" />
 </div>
 
+<br>
+
+<div>
+	<label for="Message">Message :</label>
+	<br>
+        <textarea id="tailleMessage"></textarea>  
+</div>
+
+<br>
+
+<div><input type="submit" value="envoyer"></input></div>
+
+</div>
