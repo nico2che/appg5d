e788d069bfcf28688ce732c56dcca2fb9c023776
Ohlmann Anthony <anthony.ohlmann@isep.fr>
2016-05-03 11:49:25 +0200

    Footer et Accueil

--- vues/accueil.php
+++ vues/accueil.php
@@ -13,14 +13,26 @@
 		<input type="submit" value="Rechercher">
 	</form>
 	<div class="fond"></div>
-	<h2 class="titre texteBase">Nos sports</h2>
+	<h2 class="titreImage texteBase">Nos sports</h2>
 	<div class="petitTitre texteBase">Découvrez un grand choix de sport à pratiquer entre amis</div>
 	<div id="annonces">
-		<div class = "annonces1" id="annonce1" >Football</div>
+		<div class = "annonces1" id="annonce1">Football</div>
 		<div class = "annonces1" id="annonce2">Tennis</div>
 		<div class = "annonces1" id="annonce3">Rugby</div>
 		<div class = "annonces1" id="annonce4">Basket</div>
 		<div class = "annonces1" id="annonce5">Cyclisme</div>
 		<div class = "annonces1" id="annonce6">Randonnée</div>
-
+	</div>
+	<div>
+		<input type="submit" value="Voir tous les sports" class="bouton">
+	</div>
+	<h2 class="titreClassique texteBase">Les annonces à la mode</h2>
+	<div class="petitTitre texteBase">Choisissez un groupe dynamique avec des personnes motivées</div>
+	<div id='annoncesMoment'>
+		<div class="annonces2">
+			
+		</div>
+		<div class="annonces2">
+			
+		</div>
 	</div>
