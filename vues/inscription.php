5057e18ea8f1b434d85bdc999a06bfc17251ee60
François-Xavier Bonnefont <bonnefontfx@gmail.com>
2016-05-03 12:08:22 +0200

    chgt inscription

--- vues/inscription.php
+++ vues/inscription.php
@@ -1,27 +1,32 @@
 <div id="cover"></div>
-<div class="box grad align-center">
-	<form action="" method="post">
-		<h2>Inscription</h2>
+	<div class="inscription">
+	<form class="box" action="" method="post">
+		<h1>Inscription</h1>
 		<?php if(isset($message)) echo $message; ?>
+
+		<div class="inscription-gauche">
 		<div>
-			<label>Prénom : </label>
+			<label>Prénom</label>
 			<input type="text" name="prenom" value="<?php echo (isset($_POST['prenom']) ? htmlspecialchars($_POST['prenom']) : null); ?>">
 		</div>
 		<div>
-			<label>Nom : </label>
+			<label>Nom</label>
 			<input type="text" name="nom" value="<?php echo (isset($_POST['prenom']) ? htmlspecialchars($_POST['nom']) : null); ?>">
 		</div>
 		<div>
-			<label>Email : </label>
+			<label>Email</label>
 			<input type="text" name="email" value="<?php echo (isset($_POST['prenom']) ? htmlspecialchars($_POST['email']) : null); ?>">
 		</div>
+		</div>
+		<div class="inscription-droite">
 		<div>
-			<label>Mot de passe : </label>
+			<label>Mot de passe</label>
 			<input type="password" name="mot_de_passe">
 		</div>
 		<div>
-			<label>Confirmation du mot de passe : </label>
+			<label>Confirmation</label>
 			<input type="password" name="confirmation_mot_de_passe">
+		</div>
 		</div>
 		<div>
 			<input type="submit">
