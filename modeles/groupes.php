16abbcbd3b48da395b820918bb25fadfb5f6695f
Nicolas <nico2che@gmail.com>
2016-05-06 02:30:38 +0200

    groupes

--- modeles/groupes.php
+++ modeles/groupes.php
@@ -3,8 +3,10 @@
 function recuperer_groupes() {
 
 	global $pdo;
-	$stmt = $pdo->query('SELECT g.*, s.nom AS nom_sport FROM groupes AS g LEFT JOIN sports AS s ON s.id = g.id_sport ');
-	$resultat = array();
+	$stmt = $pdo->query('SELECT s.nom AS nom_sport, m.*, g.* FROM groupes_membres AS g_m
+																LEFT JOIN groupes AS g ON g.id = g_m.id_groupe
+																LEFT JOIN membres AS m ON m.id = g_m.id_membre
+																LEFT JOIN sports AS s ON s.id = g.id_sport');
 	while($ligne = $stmt->fetch()) {
 		$resultat[] = $ligne;
 	}
@@ -14,6 +16,12 @@ function recuperer_groupes() {
 function infos_groupe($id) {
 
 	global $pdo;
-	$stmt = $pdo->query('SELECT g.*, s.nom AS nom_sport FROM groupes AS g LEFT JOIN sports AS s ON s.id = g.id_sport ');
+	$stmt = $pdo->prepare('SELECT s.nom AS nom_sport, m.*, g.* FROM groupes_membres AS g_m
+																LEFT JOIN groupes AS g ON g.id = g_m.id_groupe
+																LEFT JOIN membres AS m ON m.id = g_m.id_membre
+																LEFT JOIN sports AS s ON s.id = g.id_sport
+															WHERE g.id = :id');
+	$stmt->bindValue('id', $id, PDO::PARAM_INT);
+	$stmt->execute();
 	return $stmt->fetch();
 }
--- vues/groupe.php
+++ vues/groupe.php
@@ -1,11 +1,15 @@
 <div class="groupe">
 	<div class="photo" style="background-image: url('static/user/groupes/tennis_1.png');"></div>
 	<div class="details">
-		<h3><?php echo $infos_groupe['titre']; ?></h3>
-		<p>
-			<i class="fa fa-fire"></i> <?php echo $infos_groupe['nom_sport']; ?><br>
-			<i class="fa fa-calendar"></i> <?php echo $infos_groupe['recurrence']; ?><br>
-			<i class="fa fa-users"></i> 2 personnes sur <?php echo $infos_groupe['max_participants']; ?><br>
+		<h2><?php echo $infos_groupe['titre']; ?></h2>
+		<p class="specifications">
+			<span>Créé par <?php echo $infos_groupe['prenom']; ?></span>
+			<span><i class="fa fa-fire"></i> <?php echo $infos_groupe['nom_sport']; ?></span>
+			<span><i class="fa fa-calendar"></i> <?php echo $infos_groupe['recurrence']; ?></span>
+			<span><i class="fa fa-users"></i> 2 personnes sur <?php echo $infos_groupe['max_participants']; ?></span>
 		</p>
+		<p class="description"><?php echo nl2br($infos_groupe['description']); ?></p>
+		<h3>Dates</h3>
+		<p>Cet événement est <?php echo $infos_groupe['recurrence']; ?>.<br>Voici les dates actuellement proposées par le créateur de ce groupe.</p>
 	</div>
 </div>
