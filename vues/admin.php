79bc483347be9fe76b38bd8e4663e75ea948aa9c
François-Xavier Bonnefont <bonnefontfx@gmail.com>
2016-05-09 10:42:12 +0200

    drg

--- vues/admin.php
+++ vues/admin.php
@@ -1,18 +1,12 @@
 <div id="cover"></div>
 <div class="connexion">
 	<form class="box grad align-center" method="post">
-		<h1>Connexion</h1>
+		<h1>Administration</h1>
 		<div class="connexion-gauche">
-			<p>Vous avez déjà un compte ?</p>
+			<p>Connection en tant qu'administrateur</p>
 			<label>Identifiant</label><input type="text" name="email"><br>
 			<label>Mot de passe</label><input type="password" name="mot_de_passe"><br>
 			<input class="input" type="submit" value="Valider">
-		</div>
-		<div class="connexion-droite">
-			<a class="connexion-fb" >
-            	<span class="fa fa-facebook"></span>
-            	<span class="texte">Chargement...</span>
-          	</a>
 		</div>
 		<div class="clear"></div>
 	</form>
