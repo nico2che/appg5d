6574e66036a25fb52de037511e060e318f5ce17c
François-Xavier Bonnefont <bonnefontfx@gmail.com>
2016-05-10 10:41:38 +0200

    bdd

--- controleurs/bdd.php
+++ controleurs/bdd.php
@@ -3,14 +3,209 @@
 	$bdd = new PDO('mysql:host=localhost;dbname=app;charset=utf8','root', '');
 	$request = $bdd->prepare('SELECT * FROM (table)');
 	
-	/*foreach($request as $ligne){
-		echo"
-			<tr>
-				<th>{$ligne[1]}</th>
-				<th>{$ligne[2]}</th>
-				<th>{$ligne[3]}</th>
-				<th>{$ligne[10]}</th>
+	if(isset($_POST['table']) && !empty($_POST['table'])){
+		$request->bindParam(':table', $_POST['table']);
+		$request->execute();
+		switch($_POST['table']){
+			case 'aide':
+				echo "
+					<h1>Table sélectionnée:</h1>
+						<table>
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>Titre</th>
+									<th>Texte</th>
+								</tr>
+							</thead>
+							<tbody>";
+				break;
 
-			</tr>";
-	}*/
+				case 'clubs':
+				echo "
+					<h1>Table sélectionnée:</h1>
+						<table>
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>Nom</th>
+									<th>Description</th>
+								</tr>
+							</thead>
+							<tbody>";
+				break;
+
+				case 'commentaires_clubs':
+				echo "
+					<h1>Table sélectionnée:</h1>
+						<table>
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>ID club</th>
+									<th>ID membre</th>
+									<th>Commentaire</th>
+									<th>Note</th>
+								</tr>
+							</thead>
+							<tbody>";
+				break;
+
+				case 'contact_message':
+				echo "
+					<h1>Table sélectionnée:</h1>
+						<table>
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>Nom</th>
+									<th>E-mail</th>
+									<th>Sujet</th>
+									<th>Contenu</th>
+								</tr>
+							</thead>
+							<tbody>";
+				break;
+
+				case 'dates_rencontres':
+				echo "
+					<h1>Table sélectionnée:</h1>
+						<table>
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>ID groupe</th>
+									<th>Date</th>
+									<th>Durée</th>
+									<th>Localisation</th>
+								</tr>
+							</thead>
+							<tbody>";
+				break;
+
+				case 'forum_messages':
+				echo "
+					<h1>Table sélectionnée:</h1>
+						<table>
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>ID sujet</th>
+									<th>ID membre</th>
+									<th>Message</th>
+									<th>Date</th>
+								</tr>
+							</thead>
+							<tbody>";
+				break;
+
+				case 'forum_sujets':
+				echo "
+					<h1>Table sélectionnée:</h1>
+						<table>
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>ID membre</th>
+									<th>Type</th>
+									<th>ID sport</th>
+									<th>Résolus</th>
+									<th>Titre</th>
+									<th>Message</th>
+									<th>Date</th>
+								</tr>
+							</thead>
+							<tbody>";
+				break;
+
+				case 'groupes':
+				echo "
+					<h1>Table sélectionnée:</h1>
+						<table>
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>Titre</th>
+									<th>Description</th>
+									<th>ID sport</th>
+									<th>ID club</th>
+									<th>Max participants</th>
+									<th>Min participants</th>
+									<th>Visibilitée</th>
+									<th>Récurrence</th>
+									<th>Niveau</th>
+								</tr>
+							</thead>
+							<tbody>";
+				break;
+
+				case 'groupes_membres':
+				echo "
+					<h1>Table sélectionnée:</h1>
+						<table>
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>ID groupe</th>
+									<th>ID membre</th>
+									<th>Type</th>
+								</tr>
+							</thead>
+							<tbody>";
+				break;
+
+				case 'membres':
+				echo "
+					<h1>Table sélectionnée:</h1>
+						<table>
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>Nom</th>
+									<th>Prenom</th>
+									<th>E-mail</th>
+									<th>Mot de passe</th>
+									<th>Description</th>
+									<th>Date de naissance</th>
+									<th>Localisation</th>
+									<th>Sexe</th>
+									<th>Bannis</th>
+									<th>Role</th>
+									<th>Activité forum</th>
+								</tr>
+							</thead>
+							<tbody>";
+				break;
+
+				case 'sport':
+				echo "
+					<h1>Table sélectionnée:</h1>
+						<table>
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>Nom</th>
+									<th>Description</th>
+								</tr>
+							</thead>
+							<tbody>";
+				break;
+		}
+		
+
+		foreach($request as $ligne){
+			echo"
+				<tr>
+					<th>{$ligne[1]}</th>
+					<th>{$ligne[2]}</th>
+					<th>{$ligne[3]}</th>
+					<th>{$ligne[10]}</th>
+
+				</tr>";
+		}
+		echo "
+			</tbody>
+		</table>";
+
+	}
 ?>
--- vues/bdd.php
+++ vues/bdd.php
@@ -2,35 +2,20 @@
 <p> Ici, vous pouvez modifier la base de donnée, cela aura un effet direct sur le site. 
 Vous devez tout d'abord selectionner une table à modifier, puis le nom de la colonne à modifier.</p></br>
 <p>Choisissez la table: 
-<form>
-<select name="table" method="post" size="1">
-<option>aide
-<option>clubs
-<option>commentaires_clubs
-<option>contacte_message
-<option>dates_rencontres
-<option>forum_messages
-<option>forum_sujets
-<option>groupes
-<option>groupes_membres
-<option>membres
-<option>sports
-</select>
-<input type="submit" name="submit1" value="Afficher"></input>
-</form>
+	<form method="post">
+		<select name="table" size="1">
+			<option>aide
+			<option>clubs
+			<option>commentaires_clubs
+			<option>contacte_message
+			<option>dates_rencontres
+			<option>forum_messages
+			<option>forum_sujets
+			<option>groupes
+			<option>groupes_membres
+			<option>membres
+			<option>sports
+		</select>
+		<input type="submit" name="submit1" value="Afficher"></input>
+	</form>
 </p>
-<h1>Table sélectionnée:</h1>
-<table>
-			<thead>
-				<tr>
-					<th>Nom</th>
-					<th>Prenom</th>
-					<th>Email</th>
-					<th>Rôle</th>
-				</tr>
-			</thead>
-			<tbody>
-
-			
-			</tbody>
-</table>
