e45e9a85ac20f13950ac1b49c81a0cfa2b88ff9e
François-Xavier Bonnefont <bonnefontfx@gmail.com>
2016-05-10 09:20:09 +0200

    bdd

--- controleurs/bdd.php
+++ controleurs/bdd.php
@@ -1,4 +1,27 @@
-<h1>Users</h1>
+<h1>Bienvenue sur votre espace d'administration</h1>
+<p> Ici, vous pouvez modifier la base de donnée, cela aura un effet direct sur le site. 
+Vous devez tout d'abord selectionner une table à modifier, puis le nom de la colonne à modifier.</p></br>
+<p>Choisissez la table: 
+<form>
+<select name="table" method="post" size="1">
+<option>aide
+<option>clubs
+<option>commentaires_clubs
+<option>contacte_message
+<option>dates_rencontres
+<option>forum_messages
+<option>forum_sujets
+<option>groupes
+<option>groupes_membres
+<option>membres
+<option>sports
+</select>
+</form>
+</p>
+<h1><?php if(isset($_POST['table'])){
+			$_POST['table'];
+		}
+	?></h1>
 <table>
 			<thead>
 				<tr>
@@ -26,4 +49,3 @@
 ?>
 </tbody>
 </table>
-</div>
