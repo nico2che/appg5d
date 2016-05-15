320528fdd90128f2ecbdcb561e3fcae8434853cc
Ohlmann Anthony <anthony.ohlmann@isep.fr>
2016-05-15 16:12:50 +0200

    Page d'aide

--- vues/aide.php
+++ vues/aide.php
@@ -1,24 +1,26 @@
 <div id="cover"></div>
+
+<form class="box grad" method="post">
 <div>
-	<h1> FAQ </h1>
+	<h2 class="Title">Foire aux Questions</h2>
 </div>
-<br>
+
 <div id="cadreP" class="red-hover">
 
 	<div>
-		<a href="#" id="menu1" onclick="affichage(1)" ><h3 class="onglet">Completer son profil</h3></a>
+		<a href="#" id="menu1" onclick="affichage(1)" ><h3 class="onglet">Inscription</h3></a>
 	</div>
 	<div>
-		<a href="#" id="menu2" onclick="affichage(2)"><h3 class="onglet">Rejoindre un groupe</h3></a>
+		<a href="#" id="menu2" onclick="affichage(2)"><h3 class="onglet">Profil</h3></a>
 	</div>
 	<div>
-		<a href="#" id="menu3"onclick="affichage(3)"><h3 class="onglet">Créer un groupe</h3></a>
+		<a href="#" id="menu3"onclick="affichage(3)"><h3 class="onglet">Groupes</h3></a>
 	</div>
 	<div>
-		<a href="#" id="menu4"onclick="affichage(4)"><h3 class="onglet">utilisation du forum</h3></a>
+		<a href="#" id="menu4"onclick="affichage(4)"><h3 class="onglet">Forum</h3></a>
 	</div>
 	<div>
-		<a href="#" id="menu5"onclick="affichage(5)"><h3 class="onglet">modifier son planning</h3></a>
+		<a href="#" id="menu5"onclick="affichage(5)"><h3 class="onglet">Planning</h3></a>
 	</div>
 
 </div>
@@ -26,29 +28,30 @@
 	<div id="complete">
 		<h3 >Completer son profile</h3>
 		<p>Thalassius vero ea tempestate praefectus praetorio praesens ipse quoque adrogantis ingenii, considerans incitationem eius ad multorum augeri discrimina, non maturitate vel consiliis mitigabat, ut aliquotiens celsae potestates iras principum molliverunt, sed adversando iurgandoque cum parum congrueret, eum ad rabiem potius evibrabat, Augustum actus eius exaggerando creberrime docens, idque, incertum qua mente, ne lateret adfectans. quibus mox Caesar acrius efferatus, velut contumaciae quoddam vexillum altius erigens, sine respectu salutis alienae vel suae ad vertenda opposita instar rapidi fluminis irrevocabili impetu ferebatur.</p>
-		<img src="static/images/logo.png" height= "90px" width: "77px">
+		<img class="logo" src="static/images/logo.png" height= "90px" width: "77px">
 	</div>
 	<div id="creer">
 		<h3 >creer un groupe</h3>
 		<p>Thalassius vero ea tempestate praefectus praetorio praesens ipse quoque adrogantis ingenii, considerans incitationem eius ad multorum augeri discrimina, non maturitate vel consiliis mitigabat, ut aliquotiens celsae potestates iras principum molliverunt, sed adversando iurgandoque cum parum congrueret, eum ad rabiem potius evibrabat, Augustum actus eius exaggerando creberrime docens, idque, incertum qua mente, ne lateret adfectans. quibus mox Caesar acrius efferatus, velut contumaciae quoddam vexillum altius erigens, sine respectu salutis alienae vel suae ad vertenda opposita instar rapidi fluminis irrevocabili impetu ferebatur.</p>
-		<img src="static/images/logo.png" height= "90px" width: "77px">
+		<img class="logo" src="static/images/logo.png" height= "90px" width: "77px">
 	</div>
 	<div id="rejoindre">
 		<h3>Rejoindre un groupe</h3>
 		<p>Hacque adfabilitate confisus cum eadem postridie feceris, ut incognitus haerebis et repentinus, hortatore illo hesterno clientes numerando, qui sis vel unde venias diutius ambigente agnitus vero tandem et adscitus in amicitiam si te salutandi adsiduitati dederis triennio indiscretus et per tot dierum defueris tempus, reverteris ad paria perferenda, nec ubi esses interrogatus et quo tandem miser discesseris, aetatem omnem frustra in stipite conteres summittendo.</p>
-		<img src="static/images/logo.png" height= "90px" width: "77px">
+		<img class="logo" src="static/images/logo.png" height= "90px" width: "77px">
 	</div>
 	<div id="forum">
 		<h3>utilisation du forum</h3>
 		<p>Ut enim quisque sibi plurimum confidit et ut quisque maxime virtute et sapientia sic munitus est, ut nullo egeat suaque omnia in se ipso posita iudicet, ita in amicitiis expetendis colendisque maxime excellit. Quid enim? Africanus indigens mei? Minime hercule! ac ne ego quidem illius; sed ego admiratione quadam virtutis eius, ille vicissim opinione fortasse non nulla, quam de meis moribus habebat, me dilexit; auxit benevolentiam consuetudo. Sed quamquam utilitates multae et magnae consecutae sunt, non sunt tamen ab earum spe causae diligendi profectae.</p>
-		<img src="static/images/logo.png" height= "90px" width: "77px">
+		<img class="logo" src="static/images/logo.png" height= "90px" width: "77px">
 	</div>
 	<div id="planning">
 		<h3>modification du planning</h3>
 		<p>Intellectum est enim mihi quidem in multis, et maxime in me ipso, sed paulo ante in omnibus, cum M. Marcellum senatui reique publicae concessisti, commemoratis praesertim offensionibus, te auctoritatem huius ordinis dignitatemque rei publicae tuis vel doloribus vel suspicionibus anteferre. Ille quidem fructum omnis ante actae vitae hodierno die maximum cepit, cum summo consensu senatus, tum iudicio tuo gravissimo et maximo. Ex quo profecto intellegis quanta in dato beneficio sit laus, cum in accepto sit tanta gloria.</p>
-		<img src="static/images/logo.png" height= "90px" width: "77px">
+		<img class="logo" src="static/images/logo.png" height= "90px" width: "77px">
 	</div>
 </div>
+</form>
 
 
 	<!--<img src="static/images/logo.png" height= "90px" width: "77px">-->
--- vues/bas.php
+++ vues/bas.php
@@ -3,8 +3,8 @@
 	
 	<footer>
 	<div>
-		<a href="?page=aide" style="text-decoration: none;color: white">aide</a>
-		<a href="?page=contacte" style="text-decoration: none;color: white">contact</a>
+		<a href="?page=aide" style="text-decoration: none;color: white">Aide</a>
+		<a href="?page=contacte" style="text-decoration: none;color: white">Contact</a>
 	</div>
 	</footer>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
