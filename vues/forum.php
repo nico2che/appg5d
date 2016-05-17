aacca52912b571873603cc63ea72e8f5e62513e9
Ohlmann Anthony <anthony.ohlmann@isep.fr>
2016-05-17 10:50:34 +0200

    Forum+connexion

--- vues/forum.php
+++ vues/forum.php
@@ -1,10 +1,18 @@
 <div id="cover"></div>
+<h1>Forum</h1>
 <div class="forum">
-	<h1>Forum</h1>
+	
 	<?php if(connecte()) {  ?>
 	<a href="?page=forum&amp;ajouter" class="ajouter-sujet">Ajouter un sujet</a>
 	<?php } ?>
-	<h3>Aide</h3>
+	<div class="titre">
+		<h3 class="texteTitre1">Aide</h3>
+		<h3 class="texteTitre2">Topics</h3>
+		<h3 class="texteTitre3" >Posts</h3>
+		<h3 class="texteTitre4" >Dates</h3>
+
+
+	</div>
 	<div class="sujets">
 		<ul>
 			<li><a href="?page=forum&amp;sujet="><span class="date">Date</span><span class="nom">Nom du sujet</span></a></li>
@@ -12,7 +20,10 @@
 			<li><a href="?page=forum&amp;sujet="><span class="date">Date</span><span class="nom">Nom du sujet</span></a></li>
 		</ul>
 	</div>
-	<h3>Sports</h3>
+	<div class="titre">
+		<h3>Sports</h3>
+	</div>
+	
 	<div class="sujets">
 		<ul>
 		<?php 
