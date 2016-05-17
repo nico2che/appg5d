6b4f8fa0337ddf0e787e11ab33ccc0eedf8f713c
François-Xavier Bonnefont <bonnefontfx@gmail.com>
2016-05-17 10:54:22 +0200

    bdd

--- vues/bdd.php
+++ vues/bdd.php
@@ -1,21 +1,8 @@
 <h1>Bienvenue sur votre espace d'administration</h1>
-<p> Ici, vous pouvez modifier la base de donnée, cela aura un effet direct sur le site. 
-Vous devez tout d'abord selectionner une table à modifier, puis le nom de la colonne à modifier.</p></br>
-<p>Choisissez la table: 
-	<form method="post">
-		<select name="table" size="1">
-			<option>aide
-			<option>clubs
-			<option>commentaires_clubs
-			<option>contacte_message
-			<option>dates_rencontres
-			<option>forum_messages
-			<option>forum_sujets
-			<option>groupes
-			<option>groupes_membres
-			<option>membres
-			<option>sports
-		</select>
-		<input type="submit" name="submit1" value="Afficher"></input>
-	</form>
-</p>
+<ul>
+	<li><a href="vues/gestionBDD.php">Gestion de la base de donnée</a></li>
+	<li><a href="vues/gestionForum">Gestion du forum</a></li>
+	<li><a href="vues/gestionNewsletter">Gestion de la news-letter</a></li>
+	<li><a href="vues/gestionMembres">Gestion des membres</a></li>
+<ul>
+
---
+++ vues/gestionBDD.php
@@ -0,0 +1,16 @@
+<form method="post">
+	<select name="table" size="1">
+		<option>aide
+		<option>clubs
+		<option>commentaires_clubs
+		<option>contacte_message
+		<option>dates_rencontres
+		<option>forum_messages
+		<option>forum_sujets
+		<option>groupes
+		<option>groupes_membres
+		<option>membres
+		<option>sports
+	</select>
+	<input type="submit" name="submit1" value="Afficher"></input>
+</form>
