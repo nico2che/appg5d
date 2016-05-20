a769239ab7aaf795b37c7e9df3eca7e077b0362e
chipon77 <romain.chipon77@gmail.com>
2016-05-20 11:00:32 +0200

    club commentaire

--- modeles/clubs.php
+++ modeles/clubs.php
@@ -58,3 +58,65 @@ function info_club($idt){
 	$stmt->execute();
 	return $stmt->fetch();
 }
+
+function commentaire($idC){
+	global $pdo;
+	$stmt = $pdo-> prepare('SELECT * FROM commentaires_clubs WHERE id_club=:idC');
+	
+	$stmt->bindValue('idC', $idC, PDO::PARAM_INT);
+	$stmt->execute();
+	$resultat = array();
+
+	while($ligne = $stmt->fetch()) {
+		$resultat[] = $ligne;
+	}
+	return $resultat;
+
+}
+
+function idM_nomM($idM){
+	global $pdo;
+	$stmt = $pdo-> prepare('SELECT nom,prenom FROM membres WHERE id=:idM');
+
+	$stmt->bindValue('idM', $idM, PDO::PARAM_INT);
+	$stmt->execute();
+	return $stmt->fetch();
+}
+
+function ajouterCommentaire($idC,$idM,$Comment,$note){
+	global $pdo;
+
+	$stmt = $pdo-> prepare('INSERT INTO commentaires_clubs SET id_club= :idC, id_membre= :idM, commentaire= :comment, note= :note');
+	$stmt->bindValue('idC', $idC, PDO::PARAM_STR);
+	$stmt->bindValue('idM', $idM, PDO::PARAM_STR);
+	$stmt->bindValue('comment', $Comment, PDO::PARAM_STR);
+	$stmt->bindValue('note', $note, PDO::PARAM_STR);
+
+	if($stmt->execute())
+		return $pdo->lastInsertId();
+	else
+		return false;
+}
+
+function DejaCommenter($idM){
+	global $pdo;
+	$stmt = $pdo-> prepare('SELECT * FROM commentaires_clubs WHERE id_membre= :idM');
+	$stmt->bindValue('idM', $idM, PDO::PARAM_STR);
+
+	$stmt->execute();
+	return $stmt->fetch();
+
+
+}
+
+function modifierComment($Comment,$note,$idM){
+	global $pdo;
+		$stmt = $pdo->prepare('UPDATE commentaires_clubs SET commentaire =:comment, note= :note WHERE id_membre=:idM');
+
+		$stmt->bindValue('comment', $Comment, PDO::PARAM_STR);
+		$stmt->bindValue('note', $note, PDO::PARAM_STR);
+		$stmt->bindValue('idM', $idM, PDO::PARAM_STR);
+
+		return $stmt->execute();
+}
+
