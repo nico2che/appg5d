f64cf7f0ce1a90b66db806dbf80c9cd62cebc668
François-Xavier Bonnefont <bonnefontfx@gmail.com>
2016-05-23 11:34:08 +0200

    admin

--- vues/gestionMembres.php
+++ vues/gestionMembres.php
@@ -1,1 +1,7 @@
 <p>gestion membres</p>
+
+<form method="post">
+	<input type="text" name="pseudo" placeholder="pseudo"></input>
+	<input type="text" name="email" placeholder="email"></input>
+	<button type="submit">Rechercher</button>
+</form>
