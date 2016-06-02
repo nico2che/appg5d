841fc02d2391d5682cbac0fdfb0635a7232dd634
François-Xavier Bonnefont <bonnefontfx@gmail.com>
2016-06-02 10:33:39 +0200

    back office

--- vues/gestionNL.php
+++ vues/gestionNL.php
@@ -1,5 +1,5 @@
 <h3>Tenez vos utilisateurs au courant!</h3>
-<form method="post">
-	<textarea name="newsLetter" rows="10" cols="140" placeholder="Tapez votre News Letter"></textarea></br>
+<form style="margin-left: 100px;" method="post">
+	<textarea name="newsLetter" rows="10" cols="80" placeholder="Tapez votre News Letter"></textarea></br>
 	<button type="submit">Envoyer</button>
 </form>
